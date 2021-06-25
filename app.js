530f7e042f05a984a43866b5ccee8fdf8758cf30
Emam96 <emamsh990@gmail.com>
2021-06-25 23:58:20 +0300

    entire website lol

---
+++ app.js
@@ -0,0 +1,74 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".L1");
+
+    if (entry.isIntersecting) {
+      square.classList.add("L1-anime");
+      return;
+    }
+
+    square.classList.remove("L1-anime");
+  });
+});
+
+observer.observe(document.querySelector(".L1wrap"));
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".L2");
+
+    if (entry.isIntersecting) {
+      square.classList.add("L2-anime");
+      return;
+    }
+
+    square.classList.remove("L2-anime");
+  });
+});
+
+observer2.observe(document.querySelector(".L2wrap"));
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".L3");
+
+    if (entry.isIntersecting) {
+      square.classList.add("L3-anime");
+      return;
+    }
+
+    square.classList.remove("L3-anime");
+  });
+});
+
+observer3.observe(document.querySelector(".L3wrap"));
+
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".L4");
+
+    if (entry.isIntersecting) {
+      square.classList.add("L4-anime");
+      return;
+    }
+
+    square.classList.remove("L4-anime");
+  });
+});
+
+observer4.observe(document.querySelector(".L4wrap"));
+
+const observer5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".L5");
+
+    if (entry.isIntersecting) {
+      square.classList.add("L5-anime");
+      return;
+    }
+
+    square.classList.remove("L5-anime");
+  });
+});
+
+observer5.observe(document.querySelector(".L5wrap"));
