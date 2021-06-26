9255f0f6596556cb3bde6d6b344a9b7d21588ad4
Emam96 <emamsh990@gmail.com>
2021-06-26 12:33:35 +0300

    made the whole thing responsive

--- app.js
+++ app.js
@@ -1,32 +1,86 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const square = entry.target.querySelector(".L1");
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     const square = entry.target.querySelector(".L1");
 
-    if (entry.isIntersecting) {
-      square.classList.add("L1-anime");
-      return;
-    }
+//     if (entry.isIntersecting) {
+//       square.classList.add("L1-anime");
+//       observer2.unobserve(document.querySelector(".L1wrap"));  // this part
+//       return;
+//     }
 
-    square.classList.remove("L1-anime");
+//     square.classList.remove("L1-anime");
+//   });
+// });
+
+// observer.observe(document.querySelector(".L1wrap"));
+
+
+
+
+
+
+
+
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('L1-anime');
+      }
+    });
   });
-});
+  
+  observer.observe(document.querySelector('.L1wrap'));
 
-observer.observe(document.querySelector(".L1wrap"));
+ //////////////////////////////////////////////////////////////////
 
-const observer2 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const square = entry.target.querySelector(".L2");
 
-    if (entry.isIntersecting) {
-      square.classList.add("L2-anime");
-      return;
-    }
 
-    square.classList.remove("L2-anime");
+
+
+// const observer2 = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     const square = entry.target.querySelector(".L2");
+
+//     if (entry.isIntersecting) {
+//       square.classList.add("L2-anime");
+//       return;
+//     }
+
+//     square.classList.remove("L2-anime");
+//   });
+// });
+
+// observer2.observe(document.querySelector(".L2wrap"));
+
+
+
+
+
+const observer2 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('L2-anime');
+      }
+    });
   });
-});
+  
+  observer2.observe(document.querySelector('.L2wrap'));
 
-observer2.observe(document.querySelector(".L2wrap"));
+
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
 
 const observer3 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -43,6 +97,14 @@ const observer3 = new IntersectionObserver((entries) => {
 
 observer3.observe(document.querySelector(".L3wrap"));
 
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
 const observer4 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     const square = entry.target.querySelector(".L4");
@@ -57,6 +119,15 @@ const observer4 = new IntersectionObserver((entries) => {
 });
 
 observer4.observe(document.querySelector(".L4wrap"));
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+
 
 const observer5 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
